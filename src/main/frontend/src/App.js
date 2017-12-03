5a505943c0faf3a8594902c131901aa1e00488e7
Marcel Widmer <marzelwidmer@gmail.com>
2017-12-03 21:32:00 +0100

    initial commit

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
+
+import 'whatwg-fetch';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    handleChange = (event) => {
+        this.setState({request: event.target.value})
+    };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        var request = this.state.request.trim();
+        if (!request){
+            return;
+        }
+
+        fetch (`/api/demo?hello`)
+            .then(response => {
+                return response.text();
+            })
+            .then(body => {
+                alert(body);
+            })
+    };
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Do you hear an echo ?</h1>
+                </header>
+
+                <div>
+                    <from className="App-intro" onSubmit={this.handleSubmit}>
+                        <input type="text" onChange={this.handleChange}/>
+                        <input type="submit" value="Echo"/>
+                    </from>
+                </div>
+
+            </div>
+
+        );
+    }
 }
 
 export default App;
--- src/main/java/ch/keepcalm/react/Application.java
+++ src/main/java/ch/keepcalm/react/Application.java
@@ -1,7 +1,12 @@
 package ch.keepcalm.react;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class Application {
@@ -11,3 +16,22 @@ public class Application {
 	}
 }
 
+
+@RestController(value = "/api/demo")
+@Slf4j
+class DemoController{
+
+
+	@GetMapping
+	public ResponseEntity<Resources<String>> get(@RequestParam(value = "request", defaultValue = "Hello from DemoController") String request) {
+		log.info("get method is called.");
+		return new ResponseEntity(request, HttpStatus.OK);
+	}
+
+
+	@PatchMapping
+	public ResponseEntity<Resources<String>> patch(@RequestBody String message) {
+		log.info("patch method is called.");
+		return new ResponseEntity(message, HttpStatus.CREATED);
+	}
+}
