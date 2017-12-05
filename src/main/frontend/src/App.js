788f9f7ea7d2981ab3c39284530a4024b898abd1
Marcel Widmer <marzelwidmer@gmail.com>
2017-12-05 22:30:40 +0100

    add sample impl.

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,47 +1,32 @@
-import React, { Component } from 'react';
-
-import 'whatwg-fetch';
-
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import EmployeeList from "./components/EmployeeList";
+import Client from "./components/Client";
+import Header from "./components/Header";
 
 class App extends Component {
-    handleChange = (event) => {
-        this.setState({request: event.target.value})
-    };
-    handleSubmit = (event) => {
-        event.preventDefault();
-        var request = this.state.request.trim();
-        if (!request){
-            return;
-        }
 
-        fetch (`/api/demo?hello`)
-            .then(response => {
-                return response.text();
-            })
-            .then(body => {
-                alert(body);
-            })
-    };
+    constructor(props) {
+        super(props);
+        this.state = {employees: []};
+    }
+
+    componentDidMount() {
+        Client({method: 'GET', path: '/api/employees'}).then(response => {
+            this.setState({employees: response.entity._embedded.employees});
+        });
+    }
+
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Do you hear an echo ?</h1>
-                </header>
-
+                <Header/>
                 <div>
-                    <from className="App-intro" onSubmit={this.handleSubmit}>
-                        <input type="text" onChange={this.handleChange}/>
-                        <input type="submit" value="Echo"/>
-                    </from>
+                    <EmployeeList employees={this.state.employees}/>
                 </div>
-
             </div>
-
-        );
+        )
     }
 }
 
--- src/main/java/ch/keepcalm/react/Application.java
+++ src/main/java/ch/keepcalm/react/Application.java
@@ -1,13 +1,11 @@
 package ch.keepcalm.react;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.hateoas.Resources;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
+/**
+ * @author marcelwidmer
+ */
 @SpringBootApplication
 public class Application {
 
@@ -15,23 +13,18 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 }
-
-
-@RestController(value = "/api/demo")
-@Slf4j
-class DemoController{
-
-
-	@GetMapping
-	public ResponseEntity<Resources<String>> get(@RequestParam(value = "request", defaultValue = "Hello from DemoController") String request) {
-		log.info("get method is called.");
-		return new ResponseEntity(request, HttpStatus.OK);
-	}
-
-
-	@PatchMapping
-	public ResponseEntity<Resources<String>> patch(@RequestBody String message) {
-		log.info("patch method is called.");
-		return new ResponseEntity(message, HttpStatus.CREATED);
-	}
-}
+//
+//
+//@RestController(value = "/api/demo")
+//@Slf4j
+//class DemoController{
+//
+//
+//	@GetMapping
+//	public ResponseEntity<Resources<String>> get(@RequestParam(value = "request", defaultValue = "Hello from DemoController") String request) {
+//		log.info("get method is called. " + request);
+//		return new ResponseEntity(request, HttpStatus.OK);
+//	}
+//
+//
+//}
