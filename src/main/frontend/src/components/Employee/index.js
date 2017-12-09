ce502dcdc3f69ead395c237bb9578b971d93257b
Marcel Widmer <marzelwidmer@gmail.com>
2017-12-09 08:27:00 +0100

    work on design add detail button in table

--- src/main/frontend/src/components/Employee/index.js
+++ src/main/frontend/src/components/Employee/index.js
@@ -1,13 +1,45 @@
 import React, {Component} from "react";
+import { Button, ButtonToolbar } from 'react-bootstrap';
+
+//
+// const buttonsInstance = (
+//     <ButtonToolbar>
+//         {/* Standard button */}
+//         <Button>Default</Button>
+//
+//         {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+//         <Button bsStyle="primary">Primary</Button>
+//
+//         {/* Indicates a successful or positive action */}
+//         <Button bsStyle="success">Success</Button>
+//
+//         {/* Contextual button for informational alert messages */}
+//         <Button bsStyle="info">Info</Button>
+//
+//         {/* Indicates caution should be taken with this action */}
+//         <Button bsStyle="warning">Warning</Button>
+//
+//         {/* Indicates a dangerous or potentially negative action */}
+//         <Button bsStyle="danger">Danger</Button>
+//
+//         {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+//         <Button bsStyle="link">Link</Button>
+//     </ButtonToolbar>
+// );
 
 class Employee extends Component {
     render() {
         return (
             <tr>
-                <td><a href={this.props.employee._links.self.href}>detail</a></td>
                 <td>{this.props.employee.firstName}</td>
                 <td>{this.props.employee.lastName}</td>
                 <td>{this.props.employee.description}</td>
+                <td>
+                    <ButtonToolbar>
+                        <Button bsStyle="primary" href={this.props.employee._links.self.href}>Detail</Button>
+                    </ButtonToolbar>
+
+                </td>
             </tr>
         )
     }
--- src/main/frontend/src/components/EmployeeList/index.js
+++ src/main/frontend/src/components/EmployeeList/index.js
@@ -1,69 +1,7 @@
 import React, {Component} from "react";
 import Employee from '../Employee';
 import './style.css'
-import { Button, ButtonToolbar, Table } from 'react-bootstrap';
-
-
-const buttonsInstance = (
-    <ButtonToolbar>
-        {/* Standard button */}
-        <Button>Default</Button>
-
-        {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
-        <Button bsStyle="primary">Primary</Button>
-
-        {/* Indicates a successful or positive action */}
-        <Button bsStyle="success">Success</Button>
-
-        {/* Contextual button for informational alert messages */}
-        <Button bsStyle="info">Info</Button>
-
-        {/* Indicates caution should be taken with this action */}
-        <Button bsStyle="warning">Warning</Button>
-
-        {/* Indicates a dangerous or potentially negative action */}
-        <Button bsStyle="danger">Danger</Button>
-
-        {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
-        <Button bsStyle="link">Link</Button>
-    </ButtonToolbar>
-);
-
-
-const tableInstance = (
-    <Table striped bordered condensed hover>
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
-        </tr>
-        </tbody>
-    </Table>
-);
-
-
-
+import { Table } from 'react-bootstrap';
 
 
 class EmployeeList extends Component {
@@ -75,16 +13,14 @@ class EmployeeList extends Component {
             <Table striped bordered condensed hover>
                 <thead>
                 <tr>
-                    <th>#</th>
                     <th>First Name</th>
                     <th>Last Name</th>
-                    <th>Description</th>
+                    <th colSpan='2'>Description</th>
                 </tr>
                 </thead>
                 <tbody>
                 {employees}
                 </tbody>
-                {buttonsInstance}
             </Table>
         )
     }
